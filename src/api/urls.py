cfe97dff315ec634f22fedd5a6806bb3175d54d4
Hung5sense <hung.dalh@5sense.tech>
2025-03-18 23:27:32 +0700

    update jwt

---
+++ src/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+from .views import get_fake_data
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('data-fake/', get_fake_data, name="data_fake")
+]
--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import AuthenticationFailed
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_fake_data(request):
+    """
+        Trả về dữ liệu nếu access token hợp lệ
+    """
+    try:
+        user = request.user
+        data = {
+            "message": "Token hợp lệ",
+            "data": [
+                {"id": 1, "name": "Sản phẩm A", "price": 1000},
+                {"id": 2, "name": "Sản phẩm B", "price": 2000},
+            ]
+        }
+        return Response(data, status=200)
+    except AuthenticationFailed: 
+        refresh_token = request.COOKIES.get('refresh_token')  # Hoặc lấy từ body request
+        
+        if refresh_token:
+            try:
+                refresh = RefreshToken(refresh_token)
+                new_access_token = str(refresh.access_token)
+                return Response({"access_token": new_access_token}, status=200)
+            except:
+                return Response({"error": "Refresh token hết hạn hoặc không hợp lệ!"}, status=401)
+        else:
+            return Response({"error": "Không có refresh token, vui lòng đăng nhập lại!"}, status=401)
